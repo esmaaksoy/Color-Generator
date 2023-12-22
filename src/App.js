796f62ecd6f7300d99b18f1565c76c7e7467d7a3
esma <esma.ea.aksoy@gmail.com>
2023-12-22 11:21:00 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react"
 import "./App.css"
+import SingleColor from './SingleColor'
+import Values from 'values.js'
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  cont[error, setError] = useState(false)
+  const [list,setList] = useState([])
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+  return (
+    <>
+  <section className="container">
+    <h3>color generator</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color}/>
+    </form>
+  </section>
+  <section className="colors">
+    <h4>list goes here</h4>
+  </section>
+  </>
+  )
 }
 
 export default App
