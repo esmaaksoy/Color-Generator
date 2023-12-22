a4ca1f01987c555adbad48f3eabe7a4d0675a1f9
esma <esma.ea.aksoy@gmail.com>
2023-12-22 11:40:17 +0300

    color

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 function App() {
   const [color, setColor] = useState("");
-  cont[error, setError] = useState(false)
+  const[error, setError] = useState(false)
   const [list,setList] = useState([])
   const handleSubmit = (e)=>{
     e.preventDefault()
+    try {
+      let colors = new Values(color).all(10) //10 diffrent tons
+      setList(colors)
+    } catch (error) {
+      setError(true)
+    }
+  
   }
   return (
     <>
   <section className="container">
     <h3>color generator</h3>
     <form onSubmit={handleSubmit}>
-      <input type="text" value={color}/>
+      <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#f15025" className={`${error ? "error" : null}`}/>
+      <button className="btn" type="submit">submit</button>
     </form>
   </section>
   <section className="colors">
-    <h4>list goes here</h4>
+{list.map((color,index) => {
+  return <SingleColor key={index} {...color} index={index}/>
+})
+}
   </section>
   </>
   )
