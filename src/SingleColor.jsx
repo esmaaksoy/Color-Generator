2fea24fedaafd53c637cdf73aa731eec34ee3023
esma <esma.ea.aksoy@gmail.com>
2023-12-22 12:26:05 +0300

    default color

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,16 +1,30 @@
-import rgbToHex from "./utilis"
-import { useState } from "react";
-const SingleColor = ({rgb,weight,index,hexColor}) => {
-    const [alert,setAlert] = useState(false);
-    const bcg = rgb.join(",")
-    const hex = rgbToHex(...rgb)
+import rgbToHex from "./utilis";
+import { useEffect, useState } from "react";
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
-    <article className={`color ${index>10 && "color-light"}`} style={{backgroundColor :`rgb(${bcg})`}}>
-         <p className="percent-value">{weight}%</p>
-         <p className="color-value">{hexColor}</p>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
-   
-  )
-}
+  );
+};
 
-export default SingleColor
+export default SingleColor;
