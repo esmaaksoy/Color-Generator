afbacddca3f712dffd95e6748ef652cf2cd165a6
esma <esma.ea.aksoy@gmail.com>
2023-12-22 12:49:08 +0300

    end

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState("");
   const[error, setError] = useState(false)
-  const [list,setList] = useState(new Values("#f15025").all(10))
+  const [list,setList] = useState(new Values("#012E40").all(10))
   const handleSubmit = (e)=>{
     e.preventDefault()
     try {
@@ -21,7 +21,7 @@ function App() {
   <section className="container">
     <h3>color generator</h3>
     <form onSubmit={handleSubmit}>
-      <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#f15025" className={`${error ? "error" : null}`}/>
+      <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#012E40" className={`${error ? "error" : null}`}/>
       <button className="btn" type="submit">submit</button>
     </form>
   </section>
